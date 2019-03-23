5502f5563c1ef0bb6b202c981ba7293acaff4b8a
andrey_kustov <kustovforwork@gmail.com>
2019-03-23 13:31:28 +0300

    Страница продукта

--- application/src/components/pages/Products/Product.vue
+++ application/src/components/pages/Products/Product.vue
@@ -1,48 +1,38 @@
 <template>
   <v-container>
     <app-header></app-header>
-    <h1 class="page-title">{{ product.title }}</h1>
-    <v-layout row>
-      <v-flex xs12 md12></v-flex>
-    </v-layout>
-    <v-layout row wrap>
-      <v-flex xs12 sm12 md6 order-md1 order-sm2 order-xs2>
-        <v-card light tile flat class="card ma-1 elevation-2">
-          <v-card-text>
-            <div v-if="product.exDate">
-              <h3>Срок годности</h3>
-              <p>{{product.exDate}} дн.</p>
-            </div>
-            <h3>Состав на 100 г продукта</h3>
-            <tbody>
-              <tr>
-                <td>Калорийность</td>
-                <td class="text-xs-right">{{product.cals}} кКал</td>
-              </tr>
-              <tr>
-                <td>Белки</td>
-                <td class="text-xs-right">{{product.prots}} г</td>
-              </tr>
-              <tr>
-                <td>Жиры</td>
-                <td class="text-xs-right">{{product.fats}} г</td>
-              </tr>
-              <tr>
-                <td>Углеводы</td>
-                <td class="text-xs-right">{{product.carbs}} г</td>
-              </tr>
-            </tbody>
-          </v-card-text>
-        </v-card>
-      </v-flex>
-      <v-flex xs12 sm12 md6 order-md2 order-sm1 order-xs1 v-if="product.img">
-        <v-img :src="`/static/img/${$route.params.id}.jpg`" class="product-image ma-1 elevation-2"></v-img>
-      </v-flex>
-      <v-flex xs12 sm12 md12 order-md3 order-sm3 order-xs3 v-if="product.descr">
-        <v-card tile flat class="ma-1 pa-3 elevation-2">
-          <p class="product-description">{{ product.descr }}</p>
-        </v-card>
-      </v-flex>
+    <v-layout class="page-layout" column justify-center align-center>
+      <h1 class="page-title">{{ product.title }}</h1>
+      <v-card light tile flat class="card ma-1 elevation-2">
+        <v-card-text>
+          <div v-if="product.exDate">
+            <h3>Срок годности</h3>
+            <p>{{product.exDate}} дн.</p>
+          </div>
+          <h3>Состав на 100 г продукта</h3>
+          <tbody>
+          <tr>
+            <td>Калорийность</td>
+            <td class="text-xs-right">{{product.cals}} кКал</td>
+          </tr>
+          <tr>
+            <td>Белки</td>
+            <td class="text-xs-right">{{product.prots}} г</td>
+          </tr>
+          <tr>
+            <td>Жиры</td>
+            <td class="text-xs-right">{{product.fats}} г</td>
+          </tr>
+          <tr>
+            <td>Углеводы</td>
+            <td class="text-xs-right">{{product.carbs}} г</td>
+          </tr>
+          </tbody>
+        </v-card-text>
+      </v-card>
+      <v-btn @click="addProduct">
+        Добавить в холодильник
+      </v-btn>
     </v-layout>
   </v-container>
 </template>
@@ -80,15 +70,26 @@ export default {
         localStorage.removeItem(this.$route.params.id);
       }
     }
+  },
+  methods: {
+    addProduct() {
+      const products = JSON.parse(this.$cookie.get("products"));
+      products.push(this.product);
+      this.$cookie.set("products", JSON.stringify(products));
+    }
   }
 };
 </script>
 <style scoped>
-.product-image {
-  max-height: 500px;
-}
+  .page-title {
+    text-align: center;
+  }
 
-td {
-  width: 50%;
-}
+  .page-layout {
+    min-height: 100%;
+  }
+
+  td {
+    width: 50%;
+  }
 </style>
