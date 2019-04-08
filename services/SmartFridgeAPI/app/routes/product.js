8de0738b277301bbd944638be8a8ddd7b99a6d31
Komaricus <33253356+Komaricus@users.noreply.github.com>
2019-04-08 11:00:45 +0300

    Product routes fixed

--- services/SmartFridgeAPI/app/routes/product.js
+++ services/SmartFridgeAPI/app/routes/product.js
@@ -4,7 +4,7 @@ module.exports = (app) => {
 
   const api = app.SmartFridgeAPI.app.api.product;
 
-  app.route('products/:category').get(api.loadProducts(models.Product));
+  app.route('/products/:category').get(api.loadProducts(models.Product));
   app.route('/api/v1/products').post(api.createProduct(models.Product));
   app.route('/product/:id').delete(api.deleteProduct(models.Product));
   app.route('/product/:id').get(api.findProduct(models.Product));
