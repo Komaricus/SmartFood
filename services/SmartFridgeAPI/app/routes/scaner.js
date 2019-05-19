5c94a3e6d6062a83ceb5f7867a9eaf403297292e
Komaricus <33253356+Komaricus@users.noreply.github.com>
2019-05-19 13:03:59 +0300

    Scaner API routes fixed

--- services/SmartFridgeAPI/app/routes/scaner.js
+++ services/SmartFridgeAPI/app/routes/scaner.js
@@ -5,8 +5,8 @@ module.exports = (app) => {
   const api = app.SmartFridgeAPI.app.api.scaner;
 
   app.route('/scaner/:name').get(api.findScaner(models.Scaner));
-  app.route('/api/v1/scaner').post(api.createScaner(models.Scaner));
-  app.route('/api/v1/scaner/:name').post(api.updateScaner(models.Scaner));
+  app.route('/api/v1/scaner/:name').post(api.createScaner(models.Scaner));
+  app.route('/api/v1/scaner/update/:name').post(api.updateScaner(models.Scaner));
 
   app.route('/scaner/:name').delete(api.deleteScaner(models.Scaner));
 }
